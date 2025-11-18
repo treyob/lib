d86209999ee490819bb7788d666f7878ef5e31f1
obmmoreno <michael@overbytes.com>
2025-11-17 18:20:20 -0600

    Update obtoolbox.ps1

--- Backend/obtoolbox.ps1
+++ Backend/obtoolbox.ps1
@@ -1,7 +1,7 @@
 `Clear-Host
-# Importing the modules
+# Downloading the modules
 Write-Host "Setting temporary execution policy"
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 Write-Host "Setting TLS settings for script execution"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $testing = $false
@@ -10,7 +10,7 @@ $modulearray = @(
     "https://github.com/treyob/lib/releases/download/v0.2/ob.psm1"
 )
 $tempPath = "$env:temp\obsoftware"
-Function Import-OBModules {
+Function Get-OBModules {
     param($psm1Url)
     # Extract the file name from the URL
     $fileName = [System.IO.Path]::GetFileName($psm1Url)
@@ -21,20 +21,19 @@ Function Import-OBModules {
         New-Item -Path $tempPath -ItemType Directory -Force > $null
     } while (-not (Test-Path -Path $tempPath))
     Invoke-WebRequest -Uri $psm1Url -OutFile $psm1FilePath 
-    Import-Module -Name $psm1FilePath
-    Write-Host "Imported Module $fileName"
+    Write-Host "Downloaded Module $fileName"
 }
 if (-Not $testing) {
     ForEach ($psm1Url in $modulearray) {
-        Import-OBModules $psm1Url
+        Get-OBModules $psm1Url
     }
 }
 else {
     # If Testing is enabled, pull modules from testing repository
     Write-Warning "Testing: Enabled"
     pause
-    Import-OBModules "https://github.com/obmmoreno/obwinrepair/releases/download/obtoolbox-TUI/dentalsoftware.psm1"
-    Import-OBModules "https://github.com/obmmoreno/obwinrepair/releases/download/obtoolbox-TUI/ob.psm1"
+    Get-OBModules "https://github.com/obmmoreno/obwinrepair/releases/download/obtoolbox-TUI/dentalsoftware.psm1"
+    Get-OBModules "https://github.com/obmmoreno/obwinrepair/releases/download/obtoolbox-TUI/ob.psm1"
 }
 
 $global:sound = $null
@@ -55,6 +54,7 @@ Write-Host "Running OverBytesToolbox"
 
 try {
     # Run TUI in current terminal
+    Clear-Host
     & $filePath
 }
 finally {
